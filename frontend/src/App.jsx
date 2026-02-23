04f42b610819b5a2df1d4b9a649161f0c3847a0b
damilola ogundiran <shabzy@macbookpro.home>
2026-02-23 18:35:43 +0000

    refactor(frontend): replace hardcoded backend URL with relative API path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [product, setProduct] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch("/api/products")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch products");
+        }
+        return res.json();
+      })
+      .then((data) => setProduct(data))
+      .catch((err) => setError(err.message));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: 20 }}>
+      <h1>Products store (api test)</h1>
+      {error && <p style={{ color: "red" }}>Error: {error}</p>}
+
+      {product.length === 0 && !error ? (
+        <p>Loading products...</p>
+      ) : (
+        <ul>
+          {product.map((prod) => (
+            <li key={prod.id}>
+              {prod.name} {prod.brand} - gbp{prod.price_gbp}{" "}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
