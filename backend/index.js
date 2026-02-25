b0aaee33fddef3781fc2748ee9b54708288f3cd6
damilola ogundiran <shabzy@damilolas-MacBook-Pro.local>
2026-02-25 22:24:40 +0000

    health check added and vite base url corrected

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ const { Pool } = require("pg");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get('/health', (req, res) => {
+ res.status(200).json({ status: 'ok' });
+});
 
 const pool = new Pool({
  connectionString: process.env.DATABASE_URL,
